13399472c04a999583b1a2899efa3060754523e0
Bob Bass <bob@bbass.co>
2020-05-04 19:59:05 -0400

    initial commit 2

---
+++ main.js
@@ -0,0 +1,26 @@
+const { app, BrowserWindow } = require("electron");
+
+app
+  .whenReady()
+  .then(() => {
+    // Create a window
+    const mainWindow = new BrowserWindow({
+      backgroundColor: "#2e2c29",
+      width: 800,
+      height: 600,
+      webPreferences: {
+        nodeIntegration: true
+      }
+    });
+
+    // Load a webpage
+    mainWindow.loadFile("index.html");
+  })
+  .then(() => {
+    const contextMenu = Menu.buildFromTemplate([
+      { label: "Cool", type: "radio" }
+    ]);
+    // const tray = new Tray("/my-icon");
+    const tray = new Tray("🔥");
+    tray.setToolTip("Electron ToolTip!");
+  });
